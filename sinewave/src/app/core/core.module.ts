8c95b591577e3573194bfeac4c677145c943c72d
Todor <todorpetkov1111@gmail.com>
2022-08-15 20:11:52 +0300

    product related products and adjustments

--- sinewave/src/app/core/core.module.ts
+++ sinewave/src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
